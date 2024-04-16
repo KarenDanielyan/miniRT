37e34ea2dd5882084c9336f39594aee804906932
Karen Danielyan <karendanielyan66@gmail.com>
2024-04-17 03:14:57 +0400

    Optimized ray sphere hit function

--- src/core/hit.c
+++ src/core/hit.c
@@ -6,17 +6,22 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 18:49:55 by kdaniely          #+#    #+#             */
-/*   Updated: 2024/04/17 02:50:13 by kdaniely         ###   ########.fr       */
+/*   Updated: 2024/04/17 03:14:16 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 #include "shapes.h"
 
+/*
+ * TODO: Add comment explaining the difference between
+ * standard quadratic coefficients and ones we use here.
+*/
+
 struct s_quadratic
 {
 	float	a;
-	float	b;
+	float	h;
 	float	c;
 	float	_d;
 };
@@ -31,32 +36,26 @@ float	hit_sphere(t_sphere *self, t_ray *r)
 	t_vec3				o;
 
 	o = subst_vec3(&self->center, &r->origin);
-	q.a = vec3_dot(&r->direction, &r->direction);
-	q.b = -2 * vec3_dot(&r->direction, &o);
-	q.c = vec3_dot(&o, &o) - (self->radius * self->radius);
-	q._d = q.b * q.b - 4 * q.a * q.c;
+	q.a = vec3_length_squared(&r->direction);
+	q.h = vec3_dot(&r->direction, &o);
+	q.c = vec3_length_squared(&o) - (self->radius * self->radius);
+	q._d = q.h * q.h - q.a * q.c;
 	if (q._d < 0.0)
 		return (-1.0);
-	return (1.0);
+	return ((q.h - sqrt(q._d)) / q.a);
 }
 
 float	sphere_hit(t_point3 center, float radius, t_ray *r)
 {
 	struct s_quadratic	q;
 	t_vec3				o;
-	float				t[2];
 
 	o = subst_vec3(&center, &r->origin);
-	q.a = vec3_dot(&r->direction, &r->direction);
-	q.b = -2 * vec3_dot(&r->direction, &o);
-	q.c = vec3_dot(&o, &o) - (radius * radius);
-	q._d = q.b * q.b - 4 * q.a * q.c;
-	if (q._d >= 0)
-	{
-		t[0] = (-q.b + sqrt(q._d)) / (2 * q.a);
-		t[1] = (-q.b - sqrt(q._d)) / (2 * q.a);
-		if (t[0] > 0.0 || t[1] > 0.0)
-			return (t[1]);
-	}
-	return (-1.0);
+	q.a = vec3_length_squared(&r->direction);
+	q.h = vec3_dot(&r->direction, &o);
+	q.c = vec3_length_squared(&o) - (radius * radius);
+	q._d = q.h * q.h - q.a * q.c;
+	if (q._d < 0.0)
+		return (-1.0);
+	return ((q.h - sqrt(q._d)) / q.a);
 }
--- src/shaders/ray_shader.c
+++ src/shaders/ray_shader.c
@@ -6,7 +6,7 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/09 20:40:45 by kdaniely          #+#    #+#             */
-/*   Updated: 2024/04/17 02:57:38 by kdaniely         ###   ########.fr       */
+/*   Updated: 2024/04/17 03:02:07 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,11 +17,10 @@
 
 void	ray_shader(t_control *ctl, t_ray *r, int *pixel)
 {
-	t_color	color;
-	float	t;
+	t_color		color;
 	t_point3	p;
 	t_point3	sp;
-	
+	float		t;
 
 	(void)ctl;
 	sp = vec3(0.0, 0.0, -1.0);
@@ -30,7 +29,8 @@ void	ray_shader(t_control *ctl, t_ray *r, int *pixel)
 	if (t > 0.0)
 	{
 		color = unit_vector(subst_vec3(&p, &sp));
-		color = vec3(get_x(&color) + 1.0, get_y(&color) + 1.0, get_z(&color) + 1);
+		color = vec3(get_x(&color) + 1.0, \
+			get_y(&color) + 1.0, get_z(&color) + 1);
 		color = shrink_vec3(2, &color);
 	}
 	else

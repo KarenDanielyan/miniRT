238997be63be5df21d07b54fcb0eafa0551f94d5
Karen Danielyan <karendanielyan66@gmail.com>
2024-05-10 19:00:42 +0400

    Clamp for doubles

--- src/utils/clamp.c
+++ src/utils/clamp.c
@@ -6,13 +6,22 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/20 19:53:17 by kdaniely          #+#    #+#             */
-/*   Updated: 2024/04/21 01:35:46 by kdaniely         ###   ########.fr       */
+/*   Updated: 2024/05/10 19:00:27 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
 float	clamp(float value, float min, float max)
+{
+	if (value < min)
+		value = min;
+	if (value > max)
+		value = max;
+	return (value);
+}
+
+double	clamp_d(double value, double min, double max)
 {
 	if (value < min)
 		value = min;

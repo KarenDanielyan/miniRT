fe7b56037fe3ed9f9969069cf72922fd92ebb77d
Karen Danielyan <karendanielyan66@gmail.com>
2024-05-16 18:28:09 +0400

    We no longer need create_world().

--- src/core/main.c
+++ src/core/main.c
@@ -6,7 +6,7 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 16:04:46 by kdaniely          #+#    #+#             */
-/*   Updated: 2024/05/12 20:52:42 by kdaniely         ###   ########.fr       */
+/*   Updated: 2024/05/16 18:18:01 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,6 @@
 
 void		generate_tasks(t_control *ctl);
 static void	env_init(t_control *ctl);
-static void	create_world(t_darray *world);
 
 /**
  * Main logic:
@@ -49,7 +48,6 @@ int	main(int ac, char **av)
 		mlx_destroy_window(ctl.mlx_ptr, ctl.win_ptr);
 		exit(EXIT_FAILURE);
 	}
-	create_world(&ctl.world);
 	generate_tasks(&ctl);
 	run(&ctl);
 	mlx_hook(ctl.win_ptr, ON_DESTROY, 1L << 2, &on_destroy, &ctl);
@@ -64,23 +62,4 @@ static void	env_init(t_control *ctl)
 	ctl->worker_c = sysconf(_SC_NPROCESSORS_ONLN) - 1;
 	pthread_mutex_init(&ctl->qmux, NULL);
 	init_ui(ctl);
-}
-
-static void	create_world(t_darray *world)
-{
-	void	*shape;
-	void	*hittable;
-
-	shape = new_sphere(vec3(0.0, -100.5, -1.0), 100);
-	hittable = new_hittable(SPHERE, &hit_sphere, shape);
-	((t_hittable *)hittable)->material.color = vec3(1, 0, 0);
-	ft_darray_pushback(world, hittable);
-	free(shape);
-	free(hittable);
-	shape = new_sphere(vec3(0.0, 0.0, -1.0), 0.5);
-	hittable = new_hittable(SPHERE, &hit_sphere, shape);
-	((t_hittable *)hittable)->material.color = vec3(1, 1, 0);
-	ft_darray_pushback(world, hittable);
-	free(shape);
-	free(hittable);
 }

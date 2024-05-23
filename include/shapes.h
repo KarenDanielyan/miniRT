ed3849df9c9cca8ad56d4caa1b54623d93599746
Karen Danielyan <karendanielyan66@gmail.com>
2024-05-23 23:21:31 +0400

    Normals for cylinder

--- include/shapes.h
+++ include/shapes.h
@@ -133,7 +133,7 @@ bool	hit_cone(t_shape *self, t_ray *r, double *t);
 
 /* Surfece Normals */
 
-t_vec3	get_normal(t_ray *r, t_point3 *at, t_hittable *hit);
+t_vec3	get_normal(t_point3 *at, t_hittable *hit);
 
 /* Light sources */
 

b417464bc8f53326ab4a71919c5caa7c7ea5e838
armhakob <hakobyan.armine2004@gmail.com>
2024-06-07 18:08:00 +0400

    checkerboard

--- src/shaders/color_shader.c
+++ src/shaders/color_shader.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   color_shader.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
+/*   By: armhakob <armhakob@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/04 13:50:44 by kdaniely          #+#    #+#             */
-/*   Updated: 2024/06/04 15:31:36 by kdaniely         ###   ########.fr       */
+/*   Updated: 2024/06/07 17:23:08 by armhakob         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,6 +23,15 @@ static t_color	checker_shader(t_control *ctl, t_hitrecord *hr)
 {
 	(void)ctl;
 	(void)hr;
+	t_point2	*uv;
+	t_point3	at;
+
+	uv = NULL;
+	at = apply_transform_to_point(&hr->hit->shape.sp.wtl_matrix, &hr->at);
+	uv->x = atan2(get_x(&at), get_y(&at)) / 2 * M_PI;
+	uv->y = acos(get_z(&at)) / vec3_length(&at) / M_PI;
+	if ((int)(uv->x + uv->y) % 2 == 0)
+		return (vec3(0.1, 0.2, 0.3));
 	return (vec3(0.0, 0.0, 0.0));
 }
 

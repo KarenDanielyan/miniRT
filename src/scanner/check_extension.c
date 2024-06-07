a05034910e4de4b06b8b144d4311fe6f82008438
Karen Danielyan <karendanielyan66@gmail.com>
2024-06-07 13:03:16 +0400

    HOTFIX: check_extension()

--- src/scanner/check_extension.c
+++ src/scanner/check_extension.c
@@ -6,7 +6,7 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 18:08:59 by kdaniely          #+#    #+#             */
-/*   Updated: 2024/05/27 22:08:26 by kdaniely         ###   ########.fr       */
+/*   Updated: 2024/06/06 01:30:37 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,7 +22,12 @@ int	check_extension(char *name, char *extension)
 {
 	if (name)
 	{
-		if (ft_strcmp(ft_strchr(name, DOT), extension) == 0)
+		if (*name == DOT && *(name + 1))
+		{
+			if (ft_strcmp(ft_strchr(name + 1, DOT), extension) == 0)
+				return (EXIT_SUCCESS);
+		}
+		else if (ft_strcmp(ft_strchr(name, DOT), extension) == 0)
 			return (EXIT_SUCCESS);
 	}
 	printf("%s%s%s", RED, ERR_INVALID_EXT, RESET);

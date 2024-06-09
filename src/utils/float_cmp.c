bdcbf8639d8fee8bee8b3435fb8eda94873b23f5
Karen Danielyan <karendanielyan66@gmail.com>
2024-06-09 17:49:46 +0400

    Changing float cmp

--- src/utils/float_cmp.c
+++ src/utils/float_cmp.c
@@ -6,20 +6,15 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/06 14:28:01 by kdaniely          #+#    #+#             */
-/*   Updated: 2024/04/18 19:09:58 by kdaniely         ###   ########.fr       */
+/*   Updated: 2024/06/09 03:53:09 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "miniRT.h"
 
-static inline float	difference(float a, float b)
-{
-	return (a - b);
-}
-
 bool	float_equal(float a, float b)
 {
-	if (difference(a, b) < EPSILON)
+	if (fabs(a - b) < EPSILON)
 		return (true);
 	return (false);
 }

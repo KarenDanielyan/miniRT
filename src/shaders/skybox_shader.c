300eafef150b0a288b42abb95b6047b8a11b76eb
Karen Danielyan <karendanielyan66@gmail.com>
2024-06-09 17:50:08 +0400

    Changing base color of skybox shader

--- src/shaders/skybox_shader.c
+++ src/shaders/skybox_shader.c
@@ -6,7 +6,7 @@
 /*   By: kdaniely <kdaniely@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/15 20:47:58 by kdaniely          #+#    #+#             */
-/*   Updated: 2024/06/02 23:25:34 by kdaniely         ###   ########.fr       */
+/*   Updated: 2024/06/09 17:42:30 by kdaniely         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,13 @@
 t_color	skybox_shader(t_control *ctl, t_ray *ray)
 {
 	t_light	*ambient;
-	t_color	from;
-	t_color	to;
-	float	a;
+	t_vec3	from;
+	t_vec3	to;
+	double	a;
 
 	ambient = ft_darray_get_if(&ctl->lights, is_ambient);
 	a = 0.5 * (get_y(&ray->direction) + 1.0);
-	from = vec3(0.3, 0.3, 0.3);
+	from = vec3(0.0, 0.0, 0.0);
 	to = ambient->color;
 	return (vec3_lerp(&from, &to, (1 - a)));
 }
